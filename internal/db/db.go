6fbaa58bc912cf51372bd979d06f7393bde2d53b
TheLuminousArtemis <111784883+TheLuminousArtemis@users.noreply.github.com>
2025-06-20 17:36:41 +0000

    connection configuration

--- internal/db/db.go
+++ internal/db/db.go
@@ -11,6 +11,14 @@ func New(addr string, maxOpenConns int, maxIdleConns int, maxIdleTime string) (*
 	if err != nil {
 		return nil, err
 	}
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
